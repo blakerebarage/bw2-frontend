e993cd8d2adb5170c4097359296c96eb8ba33c19
Saymandev <devrabbi255@gmail.com>
2025-06-11 22:44:00 +0600

    Update design fixed the background img and remove the scroll bar

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -11,8 +11,13 @@ const MainLayout = () => {
           // "url('https://www.wickspin24.live/images/velki-desktop-bg.webp')",
           `url(${bgimage})`,
         backgroundSize: "cover",
+        backgroundPosition: "top",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        minHeight: "100vh",
+       
       }}
-      className="min-h-screen flex justify-center"
+      className=" flex justify-center"
     >
       <div className="hidden lg:flex w-[30%] md:w-[20%] items-center justify-center">
         <img
@@ -25,7 +30,7 @@ const MainLayout = () => {
       {/* color2:  bg-gradient-to-b from-[#0c0c0c] via-[#1a1a1a] to-[#000000] */}
       {/* me:  bg-gradient-to-br from-green-200 via-blue-300 to-green-200 */}
       {/* bg-[url('https://i.ibb.co.com/BHFQ5w9r/8868.jpg')] */}
-      <div className="w-full md:w-[60%] lg:w-[40%] xl:w-[30%] bg-[#1B1F23] bg-cover bg-center bg-no-repeat flex flex-col min-h-screen">
+      <div className="w-full md:w-[60%] lg:w-[40%] xl:w-[30%] bg-[#1B1F23] bg-cover bg-center bg-no-repeat flex flex-col h-[100vh] overflow-y-auto overflow-x-hidden scrollbar-hide">
         <Navbar />
         <ContactWidget />
         <div className="flex-grow mb-3">
